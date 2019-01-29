0adda74ad06592010997d3da97a191f86f9b6c68
Igor Rud <rud.igor23@gmail.com>
2019-01-29 21:44:16 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,3 +7,12 @@ returning << word.reverse
   end
   returning.join(" ")
   end
+
+def reverse_each_word(sentence2)
+  array = sentence2.split(" ")
+  returning = []
+  array.collect do |word|
+    returning << word.reverse
+  end
+  returning.join(" ")
+end
